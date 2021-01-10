925cfe9e83bd08b8939d863557c69530c8cf9742
Matheus Bacochina <bacochinayt@gmail.com>
2021-01-10 06:43:30 -0300

    portscanv1

---
+++ portscV1.py
@@ -0,0 +1,17 @@
+
+# para passar comandos para o so
+import socket
+
+portas = [80, 21, 23, 443, 8080]
+
+#vai rodar todos os numeros
+for porta in portas:
+    
+    #atribuindo a um objeto, o ip(AF_INET) e o tcp (SOCK_STREAM)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #coloca tempo em ver se a porta vai estar aberta
+    client.settimeout(0.10)
+    #retorna um cod
+    codigo = client.connect_ex(('bancocn.com', porta))
+    if codigo == 0:
+        print (porta, "-porta aberta")
